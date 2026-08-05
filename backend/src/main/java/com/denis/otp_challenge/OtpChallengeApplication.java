faaa4047cbb9dc454e8b856a64dc15377ad83d4b
Denís <camblorcaberodenis@gmail.com>
2026-08-05 19:00:36 +0200

    Initial commit: add project structure

---
+++ backend/src/main/java/com/denis/otp_challenge/OtpChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.denis.otp_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtpChallengeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/denis/otp_challenge/OtpChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denis.otp_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
